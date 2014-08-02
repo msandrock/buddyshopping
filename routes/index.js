f4483ec0bba01805635a985d2411f9d4c2286c06
Pascal Vomhoff <pv@localhost>
2014-08-02 13:09:55 +0200

    Add json to index.jade

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    
+        var testProduct = {
+            _id: 1,
+            name : "Leichte Jacke",
+            description : "Dies ist eine Leicht Jacke",
+            price : 1,
+            imageUrl : "/images/home/product2.jpg"
+        };
+        var products = [];
+        products.push(testProduct);
+
+    
+  res.render('index', { title: 'Express', products: products});
 });
 
 module.exports = router;
