caea9cb7c73db96c7e4305f7cbda1cff4285d944
Martin Sandrock <msandrock@gmx.de>
2014-08-02 13:37:48 +0200

    Added database lookup for items

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.database = {
-  "connectionString" : "mongodb://10.10.11.207/test"
+  "connectionString" : "mongodb://10.10.11.207/items"
 };
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,16 @@
 var express = require('express');
+var config = require('../config.js');
+var data = require('../data.js');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    
-        var testProduct = {
-            _id: 1,
-            name : "Leichte Jacke",
-            description : "Dies ist eine Leicht Jacke",
-            price : 1,
-            imageUrl : "/images/home/product2.jpg"
-        };
-        var products = [];
-        products.push(testProduct);
-
-    
-  res.render('index', { title: 'Express', products: products});
+    // Load data from db and pass it to the view
+    data.getItems(function(err, items) {
+      if(!err) {
+        res.render('index', { title: 'Express', products: items});
+      }
+    });
 });
 
 module.exports = router;
