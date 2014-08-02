e95c9f18169afe71c439e3c8b6040ebc6a0fef93
Martin Geisse <martin.geisse@shopgate.com>
2014-08-02 21:57:58 +0200

    ip address handling

--- routes/test-geisse.js
+++ routes/test-geisse.js
@@ -1,19 +1,12 @@
 var express = require('express');
 var config = require('../config.js');
 var data = require('../data.js');
+var ipAddress = require('../ipaddress.js').ipAddress;
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	data.getBuddygroupId('foobar', function(error, buddygroupId) {
-		if (error) {
-			var err = new Error(error);
-			err.status = 500;
-			next(err);
-		} else {
-			res.render('test-geisse', {buddygroupId: buddygroupId});
-		}
-	});
+	res.render('test-geisse', {ipAddress: ipAddress});
 });
 
 module.exports = router;
