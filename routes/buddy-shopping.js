ea2836cb8f344586c092e7ef45511c6a83f489b3
Pascal Vomhoff <pv@localhost>
2014-08-03 15:58:39 +0200

    fix on join username

--- routes/buddy-shopping.js
+++ routes/buddy-shopping.js
@@ -8,11 +8,11 @@ var router = express.Router();
 // Load the popup contents to start or join a buddy shopping session
 //
 router.get('/', function(req, res, next) {
+	console.log(req.query);
 	// Update the user name in session
 	if(req.query.userName) {
 		req.session.userName = req.query.userName;
 	}
-	console.log(req.session);
 
 	data.getBuddygroupId(req.session, req.sessionID, req.query.userName ,function(error, buddygroupId) {
 
@@ -34,8 +34,7 @@ router.post('/', function(req, res, next) {
 		res.redirect(req.referer || '/');
 	}
 	if (req.body.buddygroupId) {
-		data.joinBuddygroup(req.session, req.sessionID, req.body.buddygroupId, null ,function(){
-			console.log(req.session);
+		data.joinBuddygroup(req.session, req.sessionID, req.body.buddygroupId, req.session.userName ,function(){
 			redirect();
 		});
 	} else {
